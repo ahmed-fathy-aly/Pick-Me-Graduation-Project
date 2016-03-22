a1903f628517139cd08a8785e2cf70055325521f
Raafat525 <ra2fat525@gmail.com>
2016-03-22 23:57:51 +0400

    chat screen
    finish layout of chat with sender and receiver rows and mock data

--- app/src/main/java/com/asu/pick_me_graduation_project/activity/ChatActivity.java
+++ app/src/main/java/com/asu/pick_me_graduation_project/activity/ChatActivity.java
@@ -4,17 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.asu.pick_me_graduation_project.R;
 import com.asu.pick_me_graduation_project.adapter.ChatMessagesAdapter;
 import com.asu.pick_me_graduation_project.callback.GetMessagesCallback;
+import com.asu.pick_me_graduation_project.callback.GetProfileCallback;
 import com.asu.pick_me_graduation_project.controller.AuthenticationAPIController;
 import com.asu.pick_me_graduation_project.controller.ChatAPIController;
+import com.asu.pick_me_graduation_project.controller.UserApiController;
 import com.asu.pick_me_graduation_project.model.ChatMessage;
+import com.asu.pick_me_graduation_project.model.User;
 import com.asu.pick_me_graduation_project.utils.Constants;
+import com.asu.pick_me_graduation_project.utils.ValidationUtils;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -37,7 +45,8 @@ public class ChatActivity extends BaseActivity
     String userId;
     ChatAPIController controller;
     private ChatMessagesAdapter adapter;
-
+    TextView name;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -46,21 +55,28 @@ public class ChatActivity extends BaseActivity
 
         // reference views
         ButterKnife.bind(this);
-
+         name= (TextView) findViewById(R.id.receiverName);
+        image= (ImageView) findViewById(R.id.receiverPP);
+        User u=new User();
+       // u.setFirstName();
         // setup common views
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         progressBar = (ProgressBar) toolbar.findViewById(R.id.progressBar);
+        //name.setText();
 
         // get extras
         userId = getIntent().getStringExtra(Constants.USER_ID);
 
+
         // setup fields
         controller = new ChatAPIController(this);
+
 
         // TODO - setup list view and its adapter
         adapter=new ChatMessagesAdapter(this,new AuthenticationAPIController(getApplicationContext()).getCurrentUser().getUserId());
          ListViewChat.setAdapter(adapter);
+
 
         // load data
         loadMessages();
@@ -68,24 +84,54 @@ public class ChatActivity extends BaseActivity
     }
 
     private void loadMessages()
+
     {
+        UserApiController controller2 = new UserApiController(getApplicationContext());
+        controller2.getProfile(userId, new GetProfileCallback() {
+            @Override
+            public void success(User user) {
+                name.setText(ValidationUtils.correct(user.getFirstName()) + " " + ValidationUtils.correct(user.getLastName()));
+                if (ValidationUtils.notEmpty(user.getProfilePictureUrl()))
+                    Picasso.with(getApplicationContext()).
+                            load(user.getProfilePictureUrl())
+                            .placeholder(R.drawable.ic_user_small)
+                            .into(image, new Callback()
+                            {
+                                @Override
+                                public void onSuccess()
+                                {
+                                }
+
+                                @Override
+                                public void onError()
+                                {
+
+                                }
+                            });
+
+            }
+
+            @Override
+            public void fail(String message) {
+
+            }
+        });
+
+
         progressBar.setVisibility(View.VISIBLE);
         controller.getChatMessages(
                 new AuthenticationAPIController(this).getTokken()
                 , new AuthenticationAPIController(this).getCurrentUser().getUserId()
                 , userId
-                , new GetMessagesCallback()
-                {
+                , new GetMessagesCallback() {
 
                     @Override
-                    public void success(List<ChatMessage> messages)
-                    {
-                    adapter.addAll(messages);
+                    public void success(List<ChatMessage> messages) {
+                        adapter.addAll(messages);
                     }
 
                     @Override
-                    public void fail(String error)
-                    {
+                    public void fail(String error) {
 
                     }
                 }
