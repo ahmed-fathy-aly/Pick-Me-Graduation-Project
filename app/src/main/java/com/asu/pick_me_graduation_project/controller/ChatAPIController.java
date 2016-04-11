96560a81dd22742c5b07693a3fab4ee7b917ee24
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-04-11 06:53:54 +0200

    get chat from backend

--- app/src/main/java/com/asu/pick_me_graduation_project/controller/ChatAPIController.java
+++ app/src/main/java/com/asu/pick_me_graduation_project/controller/ChatAPIController.java
@@ -1,10 +1,18 @@
 package com.asu.pick_me_graduation_project.controller;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.asu.pick_me_graduation_project.callback.GetMessagesCallback;
+import com.asu.pick_me_graduation_project.callback.SendMessageCallback;
 import com.asu.pick_me_graduation_project.model.ChatMessage;
 import com.asu.pick_me_graduation_project.model.User;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -28,12 +36,61 @@ public class ChatAPIController
     /**
      * gets my recent chats
      */
-    public void getRecentChats(String token, String userId, GetMessagesCallback callback)
+    public void getRecentChats(String token, String userId, final GetMessagesCallback callback)
     {
         // TODO - get them from back end instead of mock data later
+        String url="http://pickmeasu.azurewebsites.net/api/Message/get_recent_chats"+"?count=-1";
+        Ion.with(context)
+                .load("GET",url)
+                .addHeader("Authorization", "Bearer " + token)
+                .asString().setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+                //check failed
+                if (e != null) {
+                    Log.e("Game", "error " + e.getMessage());
+                    callback.fail(e.getMessage());
+                    return;
+                }
+                //parse el response
+                Log.e("Game", "get recent chats result = " + result);
+                try {
+
+
+                    JSONObject response = new JSONObject(result);
+                    int status = response.getInt("status");
+                    if (status == 0) {
+                        String message = response.getString("message");
+                        callback.fail(message);
+                        return;
+                    }
+                    //parse
+                    JSONArray chatsJson = response.getJSONArray("chats");
+                    List<ChatMessage> messages2 = new ArrayList<ChatMessage>();
+                    for (int i = 0; i < chatsJson.length(); i++) {
+                        JSONObject chatJson = chatsJson.getJSONObject(i);
+                        ChatMessage chat = ChatMessage.fromJson(chatJson);
+                        messages2.add(chat);
+
+                    }
+                    Collections.sort(messages2, new Comparator<ChatMessage>() {
+                        public int compare(ChatMessage M1, ChatMessage M2) {
+                            return M2.getDate().compareTo(M1.getDate());
+                        }
+                    });
+                    callback.success(messages2);
+                } catch (Exception e2) {
+                    callback.fail(e2.getMessage());
+                    return;
+                }
+
+
+
+            }
+        });
 
         // make mock data
-        List<ChatMessage> messages = new ArrayList<>();
+       /* List<ChatMessage> messages = new ArrayList<>();
         for (int i = 0; i < 15; i++)
         {
             User user = new User();
@@ -68,13 +125,65 @@ public class ChatAPIController
             }
         });
 
-        callback.success(messages);
+        callback.success(messages);*/
     }
 
-    public void getChatMessages(String tokken, String myUserId, String otherUserId, GetMessagesCallback callback)
+    public void getChatMessages(String tokken, String myUserId, String otherUserId, final GetMessagesCallback callback)
     {
+        //from backend
+        String url="http://pickmeasu.azurewebsites.net/api/Message/get_a_conversation"+"?count=2"+"&userId=44";
+        Ion.with(context)
+                .load("GET",url).
+                addHeader("Authorization","Bearer"+tokken)
+                .asString().setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+                //check el fail
+                if (e != null) {
+                    Log.e("Game", "error " + e.getMessage());
+                    callback.fail(e.getMessage());
+                    return;
+                }
+                //parse el response
+                try {
+
+
+                    Log.e("Game", "get chat result = " + result);
+                    JSONObject response = new JSONObject(result);
+                    int status = response.getInt("status");
+                    if (status == 0) {
+                        String message = response.getString("message");
+                        callback.fail(message);
+                        return;
+                    }
+                    JSONArray messegesJson = response.getJSONArray("messages");
+                    List<ChatMessage> messages3 = new ArrayList<ChatMessage>();
+                    for (int i = 0; i < messegesJson.length(); i++) {
+                        JSONObject messageJson = messegesJson.getJSONObject(i).getJSONObject("message");
+                        ChatMessage message = ChatMessage.fromJson(messageJson);
+                        messages3.add(message);
+                    }
+                    //sort
+                    Collections.sort(messages3, new Comparator<ChatMessage>() {
+                        public int compare(ChatMessage M1, ChatMessage M2) {
+                            return M1.getDate().compareTo(M2.getDate());
+                        }
+                    });
+                    callback.success(messages3);
+
+                } catch (Exception e2) {
+                    callback.fail(e2.getMessage());
+                    return;
+                }
+
+
+            }
+
+        });
+
+
         // make mock data
-        List<ChatMessage> messages = new ArrayList<>();
+       /* List<ChatMessage> messages = new ArrayList<>();
         for (int i = 0; i < 15; i++)
         {
             User from = new User();
@@ -119,6 +228,58 @@ public class ChatAPIController
             }
         });
 
-        callback.success(messages);
+        callback.success(messages);*/
+    }
+    public void sendMessage(String content,String userId,String token,final SendMessageCallback callback)
+    {
+        JsonObject json = new JsonObject();
+        json.addProperty("content", content);
+        json.addProperty("userId", userId);
+        // json.addProperty("Authorization", "Bearer"+token);
+
+        String url="http://pickmeasu.azurewebsites.net/api/Message/send_message";
+
+        Ion.with(context)
+                .load(url)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization","Bearer"+token)
+                .setJsonObjectBody(json)
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                                 @Override
+                                 public void onCompleted(Exception e, String result) {
+                                     Log.e("Game", "error" + (e != null));
+                                     if (e != null) {
+                                         Log.e("Game", "error Send Message " + e.getMessage());
+                                         callback.fail(e.getMessage());
+                                         return;
+
+                                     }
+                                     Log.e("Game", "send message result = " + result);
+                                     try {
+
+
+                                         JSONObject response = new JSONObject(result);
+                                         int status = response.getInt("status");
+                                         if (status == 0) {
+                                             String message = response.getString("message");
+                                             callback.fail(message);
+                                             return;
+                                         }
+                                         JSONObject contentJson = response.getJSONObject("content");
+                                         ChatMessage message = ChatMessage.fromJson(contentJson);
+
+                                         callback.success(message);
+
+                                     } catch (Exception e2) {
+                                         callback.fail(e2.getMessage());
+                                         return;
+                                     }
+
+                                 }
+                             }
+
+                );
+
     }
 }
