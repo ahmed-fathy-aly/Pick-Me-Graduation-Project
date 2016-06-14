ebcb5b764b9a00eb3fcdf08e1eacf187da2cb3b4
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-06-14 06:18:18 +0200

    google login

--- app/src/main/java/com/asu/pick_me_graduation_project/activity/LoginActivity.java
+++ app/src/main/java/com/asu/pick_me_graduation_project/activity/LoginActivity.java
@@ -3,9 +3,11 @@ package com.asu.pick_me_graduation_project.activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
@@ -20,6 +22,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.asu.pick_me_graduation_project.R;
 import com.asu.pick_me_graduation_project.callback.LoginCallback;
@@ -32,16 +35,28 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends BaseActivity
 {
+
+    /* constants */
+    private static final int RC_SIGN_IN = 42;
 
     /* UI */
     @Bind(R.id.editTextUserEmail)
@@ -57,11 +72,14 @@ public class LoginActivity extends BaseActivity
     @Bind(R.id.content)
     LinearLayout content;
     @Bind(R.id.buttonFacebookLogin)
-    LoginButton buttonFacebookLogin;
+    Button buttonFacebookLogin;
+    @Bind(R.id.buttonGoogleSignIn)
+    Button buttonGoogleSignIn;
 
 
     /* fields */
     CallbackManager callbackManager;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -77,49 +95,84 @@ public class LoginActivity extends BaseActivity
         }
 
         super.onCreate(savedInstanceState);
-
-        // initialize facebook
         FacebookSdk.sdkInitialize(getApplicationContext());
-        LoginManager.getInstance().logOut();
-
         setContentView(R.layout.activity_login);
 
         // reference views
         ButterKnife.bind(this);
 
-        // setup facebook login
+        // init facebook
+        LoginManager.getInstance().logOut();
         callbackManager = CallbackManager.Factory.create();
-        LoginButton buttonFacebookLogin = (LoginButton) findViewById(R.id.buttonFacebookLogin);
-        buttonFacebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>()
-        {
-            @Override
-            public void onSuccess(LoginResult loginResult)
-            {
-                onFacebookLogin(loginResult.getAccessToken().getToken());
-            }
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>()
+                {
+                    @Override
+                    public void onSuccess(LoginResult loginResult)
+                    {
+                        onFacebookLogin(loginResult.getAccessToken().getToken());
+                    }
 
-            @Override
-            public void onCancel()
-            {
-            }
+                    @Override
+                    public void onCancel()
+                    {
+                    }
 
-            @Override
-            public void onError(FacebookException error)
-            {
-                Log.e("Game", "error " + error.getMessage());
-            }
-        });
+                    @Override
+                    public void onError(FacebookException exception)
+                    {
+                    }
+                });
+
+        // init google client
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener()
+                {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult)
+                    {
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN)
+        {
+            // google login
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Log.e("Game", "google result " + result.getStatus().toString());
+            if (result.isSuccess())
+            {
+                GoogleSignInAccount acct = result.getSignInAccount();
+                String idToken = acct.getIdToken();
+                onGoogleLogin(idToken);
+            }
+        }
+        else
+            callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void onFacebookLogin(String token)
+
+    @OnClick(R.id.buttonFacebookLogin)
+    void onButtonFacebookClicked()
     {
-        Log.e("Game", "token = " + token);
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "user_friends"));
+    }
+
+    @OnClick(R.id.buttonGoogleSignIn)
+    void onButtonGoogleClicked()
+    {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @OnClick(R.id.buttonLogIn)
@@ -166,6 +219,20 @@ public class LoginActivity extends BaseActivity
             }
         });
     }
+
+    private void onFacebookLogin(String token)
+    {
+        Log.e("Game", "facebook token = " + token);
+        Toast.makeText(getApplicationContext(), token, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void onGoogleLogin(String token)
+    {
+        Log.e("Game", "google token = " + token);
+        Toast.makeText(getApplicationContext(), token, Toast.LENGTH_SHORT).show();
+    }
+
 
     @OnClick(R.id.textViewSignUp)
     void goToSignUp()
