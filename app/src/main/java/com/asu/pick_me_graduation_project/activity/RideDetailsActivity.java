c942a294574845e5511993f1ad5769d7c27a9704
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-06-22 11:25:42 +0200

    share ride image

--- app/src/main/java/com/asu/pick_me_graduation_project/activity/RideDetailsActivity.java
+++ app/src/main/java/com/asu/pick_me_graduation_project/activity/RideDetailsActivity.java
@@ -1,29 +1,39 @@
 package com.asu.pick_me_graduation_project.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ActionProvider;
+import android.support.v4.view.MenuCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.asu.pick_me_graduation_project.R;
-import com.asu.pick_me_graduation_project.adapter.CommunityPagerAdapter;
 import com.asu.pick_me_graduation_project.adapter.RideDetailsPagerAdapter;
 import com.asu.pick_me_graduation_project.callback.GetRideCallback;
-import com.asu.pick_me_graduation_project.callback.GetRidesCallback;
 import com.asu.pick_me_graduation_project.controller.AuthenticationAPIController;
-import com.asu.pick_me_graduation_project.controller.CommunityAPIController;
 import com.asu.pick_me_graduation_project.controller.RidesAPIController;
 import com.asu.pick_me_graduation_project.model.Ride;
-import com.asu.pick_me_graduation_project.utils.Constants;
+import com.google.android.gms.maps.GoogleMap;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -49,6 +59,7 @@ public class RideDetailsActivity extends AppCompatActivity
     String rideId;
     RidesAPIController contoller;
     RideDetailsPagerAdapter rideDetailsPagerAdapter;
+    Ride ride;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -76,6 +87,23 @@ public class RideDetailsActivity extends AppCompatActivity
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_ride_details, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.menu_item_share)
+            shareRidePhoto();
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
     /**
      * downloads the ride info from the api
      */
@@ -91,6 +119,7 @@ public class RideDetailsActivity extends AppCompatActivity
             public void success(Ride ride)
             {
                 progressBar.setVisibility(View.INVISIBLE);
+                RideDetailsActivity.this.ride  = ride;
 
                 // check if we should show the ride join request
                 String currentUserId = new AuthenticationAPIController(RideDetailsActivity.this).getCurrentUser().getUserId();
@@ -110,8 +139,9 @@ public class RideDetailsActivity extends AppCompatActivity
 
                 // check if should open the requests tab
                 if (getIntent() != null && getIntent().getExtras() != null && getIntent().getExtras().getBoolean(SWITCH_TO_REQUEST_TAB, false))
-                    if (viewPager.getChildCount()== 3)
+                    if (viewPager.getChildCount() == 3)
                         viewPager.setCurrentItem(2, true);
+
             }
 
             @Override
@@ -122,5 +152,44 @@ public class RideDetailsActivity extends AppCompatActivity
                 Snackbar.make(content, error, Snackbar.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    /**
+     * creates an image of the ride map and details and share it
+     */
+    private void shareRidePhoto()
+    {
+        // get the bitmap to be drawn
+        rideDetailsPagerAdapter.getRideDetailsFragment().getMapsView().requestBitmap(new GoogleMap.SnapshotReadyCallback()
+        {
+            @Override
+            public void onSnapshotReady(Bitmap bitmap)
+            {
+
+                try
+                {
+                    // store to a file
+                    File file = new File(Environment.getExternalStorageDirectory(), "SharedRide" + new Random().nextInt(100000) + ".jpeg");
+                    OutputStream output = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, output);
+                    output.flush();
+                    output.close();
+                    bitmap.recycle();
+
+                    // make the share intent
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("image/jpeg");
+                    Uri uri = Uri.fromFile(file);
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                    startActivity(Intent.createChooser(shareIntent, "Share Ride"));
+
+                } catch (Exception e)
+                {
+                }
+
+            }
+        });
+
     }
 }
